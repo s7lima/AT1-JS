b68656bd99f7aa35a6e8b8d0d2e0d37cc5fd473f
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-04 22:11:11 -0300

    Feat: terminar Exercicio 2

--- at-2/js/script.js
+++ at-2/js/script.js
@@ -1,8 +1,73 @@
-function aoClicar (){
-    let campo = document.querySelector('#meuCampo');
-    let valorDigitado = campo.value;
-    alert('Você clicou! e tá escrito"' + valorDigitado + '"')
+function resultado (){
+    // Notas
+    let n = parseFloat(document.querySelector("#natureza").value);
+    let h = parseFloat(document.querySelector("#humanas").value);
+    let l = parseFloat(document.querySelector("#linguagens").value);
+    let m = parseFloat(document.querySelector("#matematica").value);
+    let r = parseFloat(document.querySelector("#redacao").value);
+
+    let resultadoN = ""
+    let resultadoH = ""
+    let resultadoL = ""
+    let resultadoM = ""
+    let resultadoR = ""
+
+    if (n > 550 ) {
+        resultadoN = "Aprovado"
+    } else if (n < 400) {
+        resultadoN = "Reprovado"
+    } else {
+        resultadoN = "Recuperação"
+    }
+
+    if (h > 550 ) {
+        resultadoH = "Aprovado"
+    } else if (h < 400) {
+        resultadoH = "Reprovado"
+    } else {
+        resultadoH = "Recuperação"
+    }
+
+    if (l > 550 ) {
+        resultadoL = "Aprovado"
+    } else if (l < 400) {
+        resultadoL = "Reprovado"
+    } else {
+        resultadoL = "Recuperação"
+    }
+
+    if (m > 550 ) {
+        resultadoM = "Aprovado"
+    } else if ( m < 400) {
+        resultadoM = "Reprovado"
+    } else {
+        resultadoM = "Recuperação"
+    }
+
+    if (r > 550 ) {
+        resultadoR = "Aprovado"
+    } else if (r < 400) {
+        resultadoR = "Reprovado"
+    } else {
+        resultadoR = "Recuperação"
+    }
+
+
+    let resultN = document.querySelector("#nResult");
+    resultN.innerHTML = resultadoN
+
+    let resultH = document.querySelector("#hResult");
+    resultH.innerHTML = resultadoH
+
+    let resultL = document.querySelector("#lResult");
+    resultL.innerHTML = resultadoL
+
+    let resultM = document.querySelector("#mResult");
+    resultM.innerHTML = resultadoM
+
+    let resultR = document.querySelector("#rResult");
+    resultR.innerHTML = resultadoR
 }
 
-let botao = document.querySelector('.btnExibaValor');
-botao.addEventListener('click', aoClicar);
+let botao = document.querySelector('.btn');
+botao.addEventListener('click', resultado);
--- at-2/js/scriptv2.js
+++
@@ -1,6 +0,0 @@
-function exibirTexto() {
-    let campo = document.querySelector("input");
-    let mensagem = campo.value;
-
-    alert("O valor digitado é: '" + mensagem + "'");
-}
